4ee14319524e4f79ff8545e87fe9b3f1c1379cb0
n1gek <tnashe.zw@gmail.com>
2025-09-15 22:39:30 -0500

    updates

---
+++ app/prof/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProfPage = () => {
+    return (
+        <main>
+            <h1>Profile Page</h1>
+            <p>Welcome to your profile page.</p>
+        </main>
+    );
+};
+
+export default ProfPage;
